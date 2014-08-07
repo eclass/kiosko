2dbd0980ad522389b656f3110ceeaf351e59c0ad
Víctor San Martín <vsanmartinm@gmail.com>
2014-08-07 18:41:03 -0400

    JSON personas, Validar RUT

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 var kiosko = function() {
 	var self = this;
 
+	var json = {
+		persons: {},
+		products: {},
+		trasactions: []
+	}
+
 	this.init = function() {
+		$.getJSON('json/personas.json', function(data) {
+			json.persons = data;
+			console.log(json);
+		});
+
 		this.home();	
 	}
 
@@ -12,10 +23,29 @@ var kiosko = function() {
 			.on('keypress', function(e) {
 				var code = e.keyCode || e.which;
 				if (code == 13) {
-					
+					var documento = $('input#documento');
+					if (!documento.val()) {
+						alert('Incluya algo');
+					}
+
+					if (!this.__isValidDocument(documento.val())) {
+						alert('Documento inválido')
+					}
 				}
 			});
 	}
+	this.__isValidDocument = function(documento) {
+		documento = documento.replace(/[^0-9kK]+/g,'').toUpperCase();
+
+		if (documento.length < 7) {
+			return false;
+		}
+
+		if (!json.persons.hasOwnerProperty(documento)) {
+			return false;
+		}
+	}
+
 
 	this.voucher = function() {
 
