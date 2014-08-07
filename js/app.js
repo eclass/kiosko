80e1f98755bbfe5e8300014b8657072c44c61229
Víctor San Martín <vsanmartinm@gmail.com>
2014-08-07 17:50:07 -0400

    Patron modular de app.ks

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
-var kiosko = {
+var kiosko = function() {
 
+	var init = function() {
+		home();	
+	}
+	init();
+
+
+	var home = function() {
+		$('documento').on('keyup', function(e) {
+			if (e.keyCode == 32) {
+				alert('enter');
+			}
+		});
+	}
+
+	var voucher = function() {
+
+	}
 
 };
 
