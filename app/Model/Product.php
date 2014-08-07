f5125ceaec90117826e7a0a072136cbed49c36c4
Sebastian Gonzalez Riffo <sebastian@gonzalezr.cl>
2014-08-07 18:44:33 -0400

    CRUD Products, using scaffold

---
+++ app/Model/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+	class Product extends Model {
+		/*public $hasOne = 'Number';
+		public $belongsTo = array(
+			    'Office' => array(
+			        'className'    => 'Office',
+			        'foreignKey'   => 'office_id'
+			    )
+			);
+		public $paginate = array(
+    		'limit' => 25,
+    		'order' => array(
+        		'Users.id' => 'asc'
+    		)
+		);*/
+	}
+
+?>
