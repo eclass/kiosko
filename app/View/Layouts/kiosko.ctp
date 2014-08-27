6dd6be6b9a263ea4bad40ba9b1f5500ff941eb16
Juan Pablo <m.juanpablob@gmail.com>
2014-08-26 21:04:57 -0400

    several changes, read commit detail
    - included front branch into kiosko view in cake
    - file structure
    - bower deployment and added to .gitignore

--- app/View/Layouts/kiosko.ctp
+++ app/View/Layouts/kiosko.ctp
@@ -1,50 +1,101 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @package       app.View.Layouts
- * @since         CakePHP(tm) v 0.10.0.1076
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
-
-$cakeDescription = __d('cake_dev', 'CakePHP: the rapid development php framework');
-$cakeVersion = __d('cake_dev', 'CakePHP %s', Configure::version())
-?>
 <!DOCTYPE html>
 <html>
-<head>
-	<?php echo $this->Html->charset(); ?>
-	<title>
-		<?php echo $cakeDescription ?>:
-		<?php echo $title_for_layout; ?>
-		KIOSKO
-	</title>
-	<?php
-		echo $this->Html->meta('icon');
+	<head>
+		<meta charset="UTF-8">
+		<title><?php echo $title_for_layout; ?></title>
 
-		echo $this->Html->css('cake.generic');
+		<link rel="stylesheet" href="<?php echo Router::url('/', true); ?>vendors/bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo Router::url('/', true); ?>vendors/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="<?php echo Router::url('/', true); ?>css/app.css">
 
-		echo $this->fetch('meta');
-		echo $this->fetch('css');
-		echo $this->fetch('script');
-	?>
-</head>
-<body>
-	<div id="container">
-		<h1>Layout Kiosko</h1>
-		<div id="content">
+		<script src="<?php echo Router::url('/', true); ?>vendors/jquery/dist/jquery.min.js"></script>
+		<script src="<?php echo Router::url('/', true); ?>js/app.js"></script>
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="logo text-center">
+						<img src="<?php $this->Html->image('logo.png'); ?>" alt="">
+					</div>
+				</div>
+			</div>
 
-			<?php echo $this->Session->flash(); ?>
+			<section id="home" class="row home">
+				<input type="text" id="passport" placeholder="RUT">
+			</section>
 
-			<?php echo $this->fetch('content'); ?>
+			<section id="cart">
+				<div class="list">
+					<input type="text" id="product_code">
+					<p class="nombre text-left">Bienvenido <span></span></p>
+					<div class="empty-list">
+						<h2>Acerca el producto al lector</h2>
+						<i class="fa fa-barcode"></i>
+					</div>
+
+					<div class="products to-show">
+						<div class="row title">
+							<div class="col-sm-6 text-left">Nombre</div>
+							<div class="col-sm-1">#</div>
+							<div class="col-sm-2">Precio</div>
+							<div class="col-sm-3">Sub-Total</div>
+							<!-- <div class="col-sm-1"></div> -->
+						</div>
+
+						<!--<div class="row">
+							<div class="col-sm-8 text-left">Papas Fritas 50gr.</div>
+							<div class="col-sm-1">1</div>
+							<div class="col-sm-2">$ 300</div>
+							<div class="col-sm-1"><a href="#"> <i class="fa fa-minus-square"></i></a></div>
+						</div>-->
+					</div>
+
+					<div class="totals to-show">
+						<div class="row">
+							<div class="col-sm-offset-7 col-sm-1"><b>Total: </b></div>
+							<div class="col-sm-4" data-kiosko="total">$0</div>
+						</div>
+					</div>
+
+					<div class="row to-show">
+						<div class="keyboards col-sm-7">
+							<div class="row">
+								<div class="col-sm-6">
+									<a href="javascript:;" class="btn btn-success confirmar">Barra Espaciadora</a>
+								</div>
+								<div class="col-sm-6">
+									Confirmar Compra
+								</div>
+							</div>
+							<!--<div class="row">
+								<div class="col-sm-6">
+									<span class="label label-info">Enter</span>
+								</div>
+								<div class="col-sm-6">
+									Agregar otro Producto
+								</div>
+							</div>-->
+							<div class="row">
+								<div class="col-sm-6">
+									<a href="javascript:;" class="btn btn-warning eliminar">Backspace</a>
+								</div>
+								<div class="col-sm-6">
+									Eliminar producto
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-6">
+									<a href="javascript:;" class="btn btn-danger cancelar">Escape</a>
+								</div>
+								<div class="col-sm-6">
+									Cancelar Compra
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</div>
-	</div>
-</body>
+	</body>
 </html>
---
+++ app/webroot/js/app.js
@@ -0,0 +1,476 @@
+var kiosko = function() {
+	var self = this,
+		state = 0,
+		cart = 0,
+		total = 0,
+		person = {},
+		idle = 10,
+		json = {
+			persons: {},
+			products: {},
+			transactions: []
+		};
+
+/*!
+ * Inicio de la magia
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.init = function() {
+		self.getPersons();
+		self.getProducts();
+
+		/*!
+		 * Atachamos el método confirm cuando presione la tecla SPACE(32)
+		 */
+		$(document).keypress(function(e) {
+			idle = 10;
+			var code = e.keyCode || e.which;
+			if (code == 32 && state == 2) {
+				self.confirm();
+			}
+		});
+		$('.confirmar').click(function(e){
+			self.confirm();
+		});
+		/*!
+		 * Atachamos el método cancel cuando precione la tecla ESC(27)
+		 */
+		$(document).keyup(function(e) {
+			idle = 10;
+			var code = e.keyCode || e.which;
+			if (code == 27 && state == 2) {
+				self.cancel();
+			}
+		});
+		$('.cancelar').click(function(e){
+			self.cancel();
+		});
+		/*!
+		 * Atachamos el método deleteProduct cuando precione la tecla BACKSPACE(8)
+		 */
+		$(document).keydown(function(e){
+			var code = e.keyCode || e.which;
+			if (code == 8 && state == 2) {
+				e.preventDefault();
+				self.deleteProduct();
+			}
+		});
+		$('.eliminar').click(function(e){
+			e.preventDefault();
+			self.deleteProduct();
+		});
+
+
+		self.initEvents();
+		self.home();
+	}
+
+/*!
+ * Se atachan los eventos a los inputs
+ *
+ * @author vsanmartin
+ * @since 2014-08-20
+ * @return void
+ */
+	this.initEvents = function() {
+		$('input#passport').on('keypress', function(e) {
+			var code = e.keyCode || e.which;
+			if (code == 13) {
+				var passport = $('input#passport');
+				self.login(passport);
+			}
+		});
+
+		$('#product_code').on('keypress', function(e) {
+			var code = e.keyCode || e.which;
+			if (code == 13) {
+				var codigo_producto = $('#product_code');
+				if (!codigo_producto.val()) {
+					alert('Acerca un producto');
+					return false;
+				}
+
+				if (self.__isValidDocument(codigo_producto.val())) {
+					self.resetCart();
+					self.login(codigo_producto);
+					return;
+				}
+
+				if (!self.__isValidProduct(codigo_producto.val())) {
+					alert('El producto no existe');
+					codigo_producto.val('');
+					return false;
+				}
+
+				self.addProduct(codigo_producto.val());
+			}
+		});
+	}
+
+/*!
+ * Muestra la primera pantalla donde la persona ingresa su rut
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.home = function() {
+		state = 1;
+		self.resetCart();
+
+		if ($('#cart').is(':visible')) {
+			$('#cart').fadeOut(function() {
+				$('#home').fadeIn(function() {
+					$('input#passport').focus().val('');
+				});
+			});
+		}
+		else {
+			$('#home').fadeIn();
+		}
+
+		$('input#passport').val('');
+	}
+
+/*!
+ * Método que valida el documento y activa el carro de compras
+ *
+ * @author vsanmartin
+ * @since 2014-08-20
+ * @param Object input
+ * @return boolean
+ */
+	this.login = function(passport) {
+		if (!passport.val()) {
+			alert('Incluya algo');
+			return false;
+		}
+
+		if (!self.__isValidDocument(passport.val())) {
+			alert('Documento inválido');
+			passport.val('');
+			return false;
+		}
+
+		person = json.persons[passport.val()];
+		self.cart();
+		passport.val('');
+		$('p.nombre span').text(person.name);
+		return true;
+	}
+
+/*!
+ * Valida si el numero passport es correcto
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @param string passport Número de passport (RUT)
+ * @return boolean
+ */
+	this.__isValidDocument = function(passport) {
+		passport = passport.replace(/[^RUT0-9kK]+/g,'').toUpperCase();
+
+		if (passport.length < 10) {
+			return false;
+		}
+		if (typeof json.persons[passport] === 'undefined') {
+			return false;
+		}
+
+		return true;
+	}
+
+/*!
+ * Limpia el carro de compras
+ *
+ * @author vsanmartin
+ * @since 2014-08-20
+ * @return void
+ */
+	this.resetCart = function() {
+		$('.products .product').remove();
+		$('div[data-kiosko="total"]').html('0');
+		$('.to-show').hide();
+		$('.empty-list').show();
+
+		total = 0;
+		person = {};
+	}
+
+/*!
+ * Muestra la segunda pantalla donde se registran los productos
+ *
+ * @author juanpablob
+ * @since 2014-08-19
+ * @return void
+ */
+	this.cart = function() {
+		state = 2;
+		self.createCart();
+
+		$('#home').fadeOut(function() {
+			$('#cart').fadeIn();
+			$('#product_code').focus();
+		});
+	}
+
+/*!
+ * Valida si el producto ingresado existe en el stock
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @param string código de producto
+ * @return boolean
+ */
+	this.__isValidProduct = function(product_code) {
+		if (typeof json.products[product_code] === 'undefined') {
+			return false;
+		}
+
+		return true;
+	}
+
+/*!
+ * Registra el producto en la transacción, actualiza el total de la compra y agrega el producto en la vista
+ *
+ * @author juanpablob
+ * @since 2014-08-19
+ * @param string código de producto
+ * @return void
+ */
+	this.addProduct = function(product_code) {
+		if ($.isEmptyObject(json.transactions[cart].products)) {
+			$('.to-show').fadeIn();
+			$('.empty-list').hide();
+		}
+
+		var quantity = 1;
+		var sub_total = parseInt(json.products[product_code].price);
+
+		if(json.transactions[cart].products[product_code]){
+			quantity  += json.transactions[cart].products[product_code].quantity;
+			sub_total += json.transactions[cart].products[product_code].total;
+		}
+
+		json.transactions[cart].products[product_code] = {quantity: quantity, total: sub_total};
+
+		total += parseInt(json.products[product_code].price);
+
+		if($('.product[data-id="'+product_code+'"]').length){
+			$('.product[data-id="'+product_code+'"] .quantity').html(quantity);
+			$('.product[data-id="'+product_code+'"] .sub-total').html('$ '+ sub_total);
+		} else {
+			$('.products').append('<div class="row product" data-id="' + product_code + '"><div class="col-sm-6 text-left name">' + json.products[product_code].name + '</div><div class="col-sm-1 text-left quantity text-center">' + json.transactions[cart].products[product_code].quantity + '</div><div class="col-sm-2">$ ' + json.products[product_code].price + '</div><div class="col-sm-2 sub-total">$ ' + json.transactions[cart].products[product_code].total + '</div><div class="col-sm-1 action"><a href="#"><i class="fa fa-minus-square"></i></a></div></div>');
+		}
+
+		$('div[data-kiosko="total"]').html('$ '+total);
+
+		$('#product_code').val('');
+
+	}
+
+/*!
+ * Elimina el producto en la transacción, actualiza el total de la compra y quita el producto en la vista
+ *
+ * @author juanpablob
+ * @since 2014-08-19
+ * @return void
+ */
+	this.deleteProduct = function() {
+		var last_product = json.transactions[cart].products.length -1;
+		last_product = json.transactions[cart].products[last_product];
+
+		total -= parseInt(json.products[last_product].price);
+		if(total == 0){
+			$('.to-show').hide();
+			$('.empty-list').show();
+		}
+		json.transactions[cart].products.pop();
+
+		$('.products .row:last-child').remove();
+
+		$('div[data-kiosko="total"]').html(total);
+	}
+
+/*!
+ * Instancia una transacción vacía con los datos del usuario
+ *
+ * @author juanpablob
+ * @since 2014-08-19
+ * @return void
+ */
+	this.createCart = function() {
+		json.transactions.push({
+			person_id: person.id,
+			products: []
+		});
+
+		cart = json.transactions.length -1;
+	}
+
+/*!
+ * Cancelar la compra
+ *
+ * Este método es llamado cuando se presiona la tecla ENTER
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return boolead
+ */
+	this.cancel = function() {
+		if (state != 2) return false;
+
+		if (confirm('¿Estás seguro de cancelar tu compra?')) {
+			json.transactions.splice(cart, 1);
+			cart--;
+
+			self.home();
+		}
+
+		return true;
+	}
+
+/*!
+ * Cuando se confirma la compra se llama este método
+ *
+ * Método accesible vía
+ * 	Tecla SPACE
+ * 	10s de iddle
+ * 	Cuando escaneen otro RUT
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.confirm = function() {
+		if (state != 2) return false;
+
+		self.home();
+	}
+
+/*!
+ * Envía todas las transacciones generadas al servidor para su registro
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.sendCarts = function() {
+		if (json.transactions.length > 0 && state == 1) {
+			$.ajax({
+				url: 'gtw/pushTransactions.php',
+				cache: false,
+				method: 'POST',
+				data: { transactions: json.transactions },
+				complete: function() {
+					json.transactions = [];
+					cart = 0;
+
+					setTimeout(function() {
+						self.sendCarts();
+					}, 5000);
+				}
+			});
+
+			return;
+		}
+
+		setTimeout(function() {
+			self.sendCarts();
+		}, 5000);
+	}
+	self.sendCarts();
+
+/*!
+ * Actualiza el json de personas
+ *
+ * Se actualiza cada 1d
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.getPersons = function() {
+		$.getJSON('gtw/persons.json', function(data) {
+			json.persons = data;
+
+			setTimeout(function() {
+				self.getPersons();
+			}, (1000 * 60 * 60 * 24));
+		});
+	}
+
+/*!
+ * Actualiza el json de productos
+ *
+ * Se actualiza cada 1m
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.getProducts = function() {
+		$.getJSON('gtw/products.json', function(data) {
+			json.products = data;
+
+			setTimeout(function() {
+				self.getProducts();
+			}, (1000 * 60));
+		});
+	}
+
+/*!
+ * Control de inactividad
+ *
+ * Si el usuario tiene mas de 10s de inactividad se toma como confirmada la compra
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.idleTime = function() {
+		if (state == 2) {
+			idle--;
+
+			if (idle <= 0) {
+				self.confirm();
+				idle = 10;
+			}
+		}
+		else {
+			idle = 10;
+		}
+
+		setTimeout(function() {
+			self.idleTime();
+		}, 1000);
+	}
+	// self.idleTime();
+
+/*!
+ * Mantiene el foco siempre en el input correcto de acuerdo al estado
+ *
+ * @author vsanmartin
+ * @since 2014-08-19
+ * @return void
+ */
+	this.autoFocus = function() {
+		if (state == 1) {
+			$('input#passport').focus();
+		}
+		else if (state == 2) {
+			$('#product_code').focus();
+		}
+
+		setTimeout(function() {
+			self.autoFocus();
+		}, 500);
+	}
+	self.autoFocus();
+
+	self.init();
+};
+
+$(document).ready(kiosko);
